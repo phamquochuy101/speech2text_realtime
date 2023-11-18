55d90265c6b11c1f9fbcd7cd89399ab4ad435b04
phamquochuy101 <phmquochuy@gmail.com>
2023-11-18 23:27:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from gtts import gTTS
 import streamlit as st
 from io import BytesIO
 from audio_recorder_streamlit import audio_recorder
-from streamlit.script_run_context import get_script_run_ctx as get_report_ctx
+from streamlit.report_thread import get_report_ctx
 
 # Set page configuration
 st.set_page_config(
